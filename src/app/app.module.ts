8c3fbaad9ec6641e1d63f2790a3814c1cb29fe6f
Keshav885 <kesava.paruchuri@aurotechcorp.com>
2017-08-01 11:09:00 -0400

    added 2 new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import {ServerComponent} from './Server/server.component';
 import { ServersComponent } from './servers/servers.component';
+import { Comp1Component } from './comp1/comp1.component';
+import { Comp2Component } from './comp2/comp2.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
-    ServersComponent
+    ServersComponent,
+    Comp1Component,
+    Comp2Component
   ],
   imports: [
     BrowserModule
